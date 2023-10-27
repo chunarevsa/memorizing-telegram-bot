1efa4e1b1c81a623693fa00a445a818a387f05ae
Chunarev Sergei <chunarev.se.a@gmail.com>
2023-10-27 17:52:18 +0200

    add menuFactory

---
+++ dispatcher/src/main/java/org/memorizing/model/menu/MenuFactory.java
@@ -0,0 +1,21 @@
+package org.memorizing.model.menu;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
+public interface MenuFactory {
+
+    EMenu getLastMenu();
+
+    EMenu getCurrentMenu();
+
+    ReplyKeyboardMarkup getKeyboard();
+    String getInfoText();
+    String getText();
+
+    InlineKeyboardMarkup getInlineKeyboard();
+
+    InlineKeyboardMarkup createInlineKeyboard(String[][] cardStockTypes);
+
+    String getName();
+}
