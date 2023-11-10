719807541b15edf7361833cdc82bf58afb7f3968
Chunarev Sergey <sergey.chunarev@ural.rt.ru>
2023-11-10 21:43:54 +0500

    add db migration

--- dispatcher/src/main/java/org/memorizing/entity/UserState.java
+++ dispatcher/src/main/java/org/memorizing/entity/UserState.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import static org.memorizing.model.EMode.*;
 
 @Entity
+@Table(name = "user_state")
 public class UserState {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
